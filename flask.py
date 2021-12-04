ae7ea22ada7ff6bf9c6121f33e615b6173da2df7
dian <72053818+9yul@users.noreply.github.com>
2021-12-04 23:02:10 +0900

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,9 +1,10 @@
-from flask import Flask
+# -- coding: utf-8 --
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def w():
-    return "hello world!" 
+def index():
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run()
+if __name__ =='__main__':
+    app.run(debug=True)
